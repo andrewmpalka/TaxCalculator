7f2c1fb82142756d8881f3ffa7eded6046c78887
Andy Palka <andrewmpalka@gmail.com>
2016-01-05 12:42:29 -0600

    Initial Commit

---
+++ TaxCalclulator App/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TaxCalclulator App
+//
+//  Created by Andrew Palka on 1/5/16.
+//  Copyright © 2016 Andrew Palka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
